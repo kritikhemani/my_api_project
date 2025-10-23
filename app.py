bfa7ac2e29751aa4dc48cca9c6a33ebaff464302
Kriti Khemani <kritikhemani04@gmail.com>
2025-10-23 18:30:15 +0530

    Update changes

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/')
 def hello():
-    return jsonify(message="Hello!"), 200
+    return jsonify(message="Hello, world!"), 200
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    data = request.get_json()
+    return jsonify(data), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
