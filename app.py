72b7334a7de3908b4b9a1d6dfba0ed624be3b901
Kriti Khemani <kritikhemani04@gmail.com>
2025-10-25 11:29:43 +0530

    Making changes

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def hello():
-    return jsonify(message="Hello, world!"), 200
+    return jsonify({"message": "Hello, world!"}), 200
 
 @app.route('/echo', methods=['POST'])
 def echo():
     data = request.get_json()
-    return jsonify(data), 201
+    return jsonify({"you sent": data}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
