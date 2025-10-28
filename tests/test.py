d67035bacb424de1b400dba05ced58faac184fcf
Kriti Khemani <kritikhemani04@gmail.com>
2025-10-28 17:01:49 +0530

    Adding function

--- tests/test.py
+++ tests/test.py
@@ -5,3 +5,10 @@ from app import app
 def client():
     app.testing = True
     return app.test_client()
+
+def test_hello(client):
+    res = client.get("/")
+    assert res.status_code == 200
+    assert res.get_json() == {"message": "Hello, world"}
+    
+    
