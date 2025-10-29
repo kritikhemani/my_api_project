ab7c7a83be1bd7dcfc3abf482b13993320fc094d
Kriti Khemani <kritikhemani04@gmail.com>
2025-10-29 12:17:23 +0530

    Adding changes

--- tests/test.py
+++ tests/test.py
@@ -10,5 +10,10 @@ def test_hello(client):
     res = client.get("/")
     assert res.status_code == 200
     assert res.get_json() == {"message": "Hello, world"}
-    
+  
+def test_echo(client):
+    res = client.post("/echo", json={"name": " "})
+    assert res.status_code == 201
+    assert res.get_json() == {"you_sent": {"name": " "}}
+  
     
